75137544e21737383d82aa637cc2024e30b970cc
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2021-01-05 22:23:05 +0300

    Moved xml utils

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from bz2 import BZ2File
-import re
 from typing import NoReturn
 from lxml.etree import iterparse, _Element
 import pathlib
-from itertools import islice
 import json
 from typing import Union
-from config import DUMP_PATH, TAGS, ARTICLE_NAMESPACE, TEMPLATE_NAMESPACE,\
-                      TMP_PATH, PROCESS_ARTICLES, PROCESS_TEMPLATES
+from config import DUMP_PATH, TMP_PATH, PROCESS_ARTICLES, PROCESS_TEMPLATES
 from src.data import Article, Template
+from src.xml_utils import is_article_title_ru, is_template, is_template_title_ru, is_redirect, is_article, \
+    is_article_title_proper, is_element_page, get_page_id, get_raw_wiki, get_page_title
 
 
 def store_raw_wiki(page: Union[Article, Template]) -> NoReturn:
@@ -31,52 +30,6 @@ def store_index2title(i2t) -> NoReturn:
     path = pathlib.Path(TMP_PATH + "index2title.json")
     with open(path, "w") as file:
         json.dump(i2t, file, indent=4)
-
-
-def is_article_title_ru(title: str) -> bool:
-    pattern = re.compile(r"^[А-Яа-яЁё\-]+[^\-]$")
-    return bool(re.match(pattern, title))
-
-
-def is_template(page: _Element) -> bool:
-    return page.find(TAGS["namespace"]).text == TEMPLATE_NAMESPACE
-
-
-def is_template_title_ru(title: str) -> bool:
-    pattern = re.compile(r"^Шаблон:.*ru.*$")
-    return (bool(re.match(pattern, title))
-            and "User" not in title)
-
-
-def is_redirect(page: _Element) -> bool:
-    if page.find(TAGS["redirect"]) is not None:
-        return True
-    else:
-        return False
-
-
-def is_article(page: _Element) -> bool:
-    return page.find(TAGS["namespace"]).text == ARTICLE_NAMESPACE
-
-
-def is_article_title_proper(title: str) -> bool:
-    return title.istitle()
-
-
-def is_element_page(elem: _Element) -> bool:
-    return "page" in elem.tag
-
-
-def get_page_id(page: _Element) -> int:
-    return int(page.find(TAGS["id"]).text)
-
-
-def get_raw_wiki(page: _Element) -> str:
-    return page.find(TAGS["text"]).text
-
-
-def get_page_title(page: _Element) -> str:
-    return page.find(TAGS["title"]).text
 
 
 def process_element(element: _Element) -> Union[Article, Template, None]:
---
+++ src/xml_utils.py
@@ -0,0 +1,51 @@
+import re
+
+from lxml.etree import _Element
+
+from config import TAGS, TEMPLATE_NAMESPACE, ARTICLE_NAMESPACE
+
+
+def is_article_title_ru(title: str) -> bool:
+    pattern = re.compile(r"^[А-Яа-яЁё\-]+[^\-]$")
+    return bool(re.match(pattern, title))
+
+
+def is_template(page: _Element) -> bool:
+    return page.find(TAGS["namespace"]).text == TEMPLATE_NAMESPACE
+
+
+def is_template_title_ru(title: str) -> bool:
+    pattern = re.compile(r"^Шаблон:.*ru.*$")
+    return (bool(re.match(pattern, title))
+            and "User" not in title)
+
+
+def is_redirect(page: _Element) -> bool:
+    if page.find(TAGS["redirect"]) is not None:
+        return True
+    else:
+        return False
+
+
+def is_article(page: _Element) -> bool:
+    return page.find(TAGS["namespace"]).text == ARTICLE_NAMESPACE
+
+
+def is_article_title_proper(title: str) -> bool:
+    return title.istitle()
+
+
+def is_element_page(elem: _Element) -> bool:
+    return "page" in elem.tag
+
+
+def get_page_id(page: _Element) -> int:
+    return int(page.find(TAGS["id"]).text)
+
+
+def get_raw_wiki(page: _Element) -> str:
+    return page.find(TAGS["text"]).text
+
+
+def get_page_title(page: _Element) -> str:
+    return page.find(TAGS["title"]).text
