ce2975c2ba6d7c92ee7a1102f4d97b69a0787ca8
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2021-01-05 22:30:29 +0300

    Move settings to yaml

--- config.py
+++ config.py
@@ -1,8 +1,18 @@
 from typing import Dict
+from yaml import safe_load
 
-DUMP_PATH: str = "data/ruwiktionary-20201201-pages-articles.xml.bz2"
-TMP_PATH: str = "tmp/"
-WIKI_NS: str = "{http://www.mediawiki.org/xml/export-0.10/}"
+
+# DUMP_PATH: str = "data/ruwiktionary-20201201-pages-articles.xml.bz2"
+# TMP_PATH: str = "tmp/"
+# WIKI_NS: str = "{http://www.mediawiki.org/xml/export-0.10/}"
+
+
+with open("config.yml") as file:
+    config = safe_load(file)
+
+DUMP_PATH = config["dump_path"]
+TMP_PATH = config["tmp_path"]
+WIKI_NS = config["wiki_ns"]
 
 TAGS: Dict[str, str] = {
     "text": WIKI_NS + "revision//" + WIKI_NS + "text",
